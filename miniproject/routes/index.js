8bd63ba6669a4228be0b06bb94701a9e3140078e
KJ Wilson <yourboykjdev@gmail.com>
2019-06-13 10:40:13 -0700

    ttd classwork

---
+++ miniproject/routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+// var venue = require('../models/venue')
+
+// var venue = {
+//   "events" : [
+//     {
+//       "band" : "Smashing Pumpkins",
+//       "date" : "September 20"
+//     },
+//     {
+//       "band" : "Pavement",
+//       "date" : "September 22"
+//     }
+//   ],
+//   "seats" : 200
+// }
+
+
+module.exports = router;
